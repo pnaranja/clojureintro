15f293802d9e58bce26f492f7d9224e9c6de2fb2
pnaranja <paul.naranja@gmail.com>
2015-09-24 01:52:20 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojureintro "0.1.0-SNAPSHOT"
+  :description "LispCast Introduction Project"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main clojureintro.core
+  )
